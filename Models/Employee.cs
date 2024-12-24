4db1c1dc8714a0f6d643190651524646f49629d8
Osama <osamaashkanani@gmail.com>
2024-12-24 15:21:54 +0300

    v0

---
+++ Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.Models
+{
+    public class Employee
+    {
+        //use prop tab tab to create properties
+        public string Name, Email, Gender;
+        public string Check(string name, string email, string gender)
+        {
+            string msg;
+
+            if (gender.Trim().ToUpper() == "M")
+            {
+                Console.Clear();
+                msg = $"... Welcome Mr. {name}\n\nEmail: {email}";
+                return msg;
+            }
+            else if (gender.Trim().ToUpper() == "F")
+            {
+                Console.Clear();
+                msg = $"... Welcome Ms. {name}\n\nEmail: {email}";
+                return msg;
+            }
+            else
+            {
+                Console.Clear();
+                msg = "... Invalid Input!";
+                return msg;
+            }
+        }
+        public string Check(string name, string family, string email, string gender)
+        {
+            return name+family+email+gender;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using System.Xml.Linq;
+using ConsoleApp8.Models;
+//       obj
+Employee employee = new Employee();
+
+employee.Name = "John Doe";
+employee.Email = "ex@gmail.com";
+
+Console.WriteLine($"Name: {employee.Name} | Email: {employee.Email}");
+
+//___________________________________________________________________________________________
+string Name, Email, Gender;
+
+Console.Write($"\nEnter your name: ");
+Name = Console.ReadLine().Trim();
+
+Console.Write($"Enter your email: ");
+Email = Console.ReadLine().Trim();
+
+Console.Write($"Your gender [M/f]: ");
+Gender = Console.ReadLine();
+
+Console.WriteLine(employee.Check(Name, Email, Gender));
+Console.WriteLine("\n" + employee.Check("Aboudy\t", "Herz\t", "AboudyHerz@gmail.ch\t", "Male\n"));
+
+//___________________________________________________________________________________________
